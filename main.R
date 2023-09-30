9ea0e9cb6a834c54f8acec789921837fe530b268
Prithvi Adiga <111407253+prithviadiga@users.noreply.github.com>
2023-09-30 16:31:01 -0400

    Update main.R

--- main.R
+++ main.R
@@ -7,3 +7,13 @@ nrow(main_doc)
 ncol(main_doc)
 # 15
 names(main_doc)
+console_names <- unique(main_doc$Console)
+
+nostolgic_year_number <- substr(main_doc$Release.Date, nchar(main_doc$Release.Date) - 1, nchar(main_doc$Release.Date))
+nostologic_year_filter <- c()
+for(i in seq_along(nostolgic_year_number)){
+  year <- as.numeric(nostolgic_year_number[i])
+  if(year > 30 || year < 10){
+    nostolgic_year_filter <- append(nostolgic_year_filter, year)
+  }
+}
