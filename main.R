9529ad0591b0980edee1eefebf67a6f418127063
Prithvi Adiga <111407253+prithviadiga@users.noreply.github.com>
2023-09-30 12:02:00 -0400

    added a few stats

--- main.R
+++ main.R
@@ -1,2 +1,9 @@
 #CDC 2023
 main_doc <- read.csv("/Users/prithviadiga/Desktop/cdc2023/cdc2023/Business_Dataset.csv", header = TRUE)
+View(main_doc)
+
+nrow(main_doc)
+# 62747
+ncol(main_doc)
+# 15
+names(main_doc)
